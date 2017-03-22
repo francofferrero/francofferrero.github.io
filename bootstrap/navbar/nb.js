3dfa17f67f207902c1b1ab25d030f217e66d18f2
francofferrero <francofferrero@gmail.com>
2017-03-22 17:20:49 -0300

    Create nb.js

---
+++ bootstrap/navbar/nb.js
@@ -0,0 +1,23 @@
+/* playlist.js */
+
+window.onload = init;
+
+function init() {
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+
+	loadPlaylist();
+}
+
+function handleButtonClick(e) {
+	
+  var textInput = "songTextInput";
+	
+			alert("Adding " + textInput);
+		
+	
+}
+
+
+    Contact GitHub API Training Shop Blog About 
+
