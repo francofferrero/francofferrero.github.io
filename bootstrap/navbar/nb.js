8ca5d4efecf5d5eb7fb5687e4b818e81d81a1be4
francofferrero <francofferrero@gmail.com>
2017-03-22 17:25:54 -0300

    Update nb.js

--- bootstrap/navbar/nb.js
+++ bootstrap/navbar/nb.js
@@ -5,8 +5,7 @@ window.onload = init;
 function init() {
 	var button = document.getElementById("addButton");
 	button.onclick = handleButtonClick;
-
-	loadPlaylist();
+	
 }
 
 function handleButtonClick(e) {
